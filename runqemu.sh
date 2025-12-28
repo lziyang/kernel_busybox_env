7ffe329ecb27411bd57a0635fe5eace9a0e772cc
lziyang <zl4223@ic.ac.uk>
2025-12-28 15:54:04 +0800

    Update runqemu.sh filepaths

--- runqemu.sh
+++ runqemu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 qemu-system-x86_64 \
-  -kernel linux-6.18.1/arch/x86/boot/bzImage \
-  -initrd initramfs.cpio.gz \
+  -kernel linux-6.18.2/arch/x86/boot/bzImage \
+  -initrd initramfs/initramfs.cpio.gz \
   -append "console=ttyS0 rdinit=/init" \
   -nographic
 
